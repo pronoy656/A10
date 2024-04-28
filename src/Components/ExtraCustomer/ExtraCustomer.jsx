a3cab141e48ba77ba26f4024dfe0a8293d64cb99
pronoy656 <pronoypaul56@gmail.com>
2024-04-28 20:44:30 +0600

    footer design add

--- src/Components/ExtraCustomer/ExtraCustomer.jsx
+++ src/Components/ExtraCustomer/ExtraCustomer.jsx
@@ -1,7 +1,6 @@
 const ExtraCustomer = () => {
   return (
     <div>
-      <h1>THis is extra sctiionknkjn</h1>
       <div className="mt-10 mb-9">
         <div className="text-center">
           <h1 className="text-[#ffa135] text-3xl font-semibold">
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -31,8 +31,8 @@ const Home = () => {
       <Banner></Banner>
 
       {/* load data and map */}
-      <p className="text-center text-3xl font-extrabold mb-6">
-        Craft items section
+      <p className="text-center text-3xl font-extrabold mb-14 mt-12">
+        Our Collection
       </p>
       <div className="grid grid-cols-3 gap-x-4 gap-y-4">
         {allArtAndCarts.map((singleArtAndCart) => (
