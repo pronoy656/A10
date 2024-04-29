6a458122ea96656a07344ef6e66810a7b99b6ba1
pronoy656 <pronoypaul56@gmail.com>
2024-04-29 22:26:57 +0600

    successfully add item on my art and craft list

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,16 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import Banner from "../Banner/Banner";
 import SingleArtAndCart from "../SingleArtAndCart/SingleArtAndCart";
 import ExtraCustomer from "../ExtraCustomer/ExtraCustomer";
 import { Typewriter } from "react-simple-typewriter";
 import ExtraImage from "../ExtraImage/ExtraImage";
 import ExtraRate from "../ExtarRate/ExtraRate";
+import { useState } from "react";
 
 const Home = () => {
   const allArtAndCarts = useLoaderData();
+  // for slice
+  const [dataLength, setDataLength] = useState(6);
   return (
     <div>
       <div className="text-center mb-14 mt-10">
@@ -38,13 +41,18 @@ const Home = () => {
         Our Collection
       </p>
       <div className="grid grid-cols-3 gap-x-4 gap-y-4">
-        {allArtAndCarts.map((singleArtAndCart) => (
+        {allArtAndCarts.slice(0, dataLength).map((singleArtAndCart) => (
           <SingleArtAndCart
             key={singleArtAndCart._id}
             singleArtAndCart={singleArtAndCart}
           ></SingleArtAndCart>
         ))}
       </div>
+      <Link to={"/allArtAndCraftItems"}>
+        <div className="flex justify-center mt-5">
+          <button className="btn btn-outline">Show All</button>
+        </div>
+      </Link>
       <ExtraImage></ExtraImage>
       <ExtraCustomer></ExtraCustomer>
     </div>
--- src/Components/MyArtAndCraftList/MyArtAndCraftList.jsx
+++ src/Components/MyArtAndCraftList/MyArtAndCraftList.jsx
@@ -1,4 +1,17 @@
+import { useContext, useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+
 const MyArtAndCraftList = () => {
+  const { user } = useContext(AuthContext);
+  const [usersItem, setUsersItem] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/MyArtAndCrafts/${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUsersItem(data);
+      });
+  }, []);
   return (
     <div>
       <h1 className="text-red-600">My Art And Craft List</h1>
@@ -17,6 +30,24 @@ const MyArtAndCraftList = () => {
             <a>Item 2</a>
           </li>
         </ul>
+      </div>
+      <div>
+        {usersItem.map((userItem) => (
+          <div key={userItem._id}>
+            <img
+              className="w-96 h-72 rounded-2xl"
+              src={userItem.image}
+              alt=""
+            />
+            <h1>Item:{userItem.item}</h1>
+            <h1>Price:{userItem.price}</h1>
+            <h1>rating:{userItem.rating}</h1>
+            <h1>Customized:{userItem.customize}</h1>
+            <h1>Stock-Status:{userItem.stockStatus}</h1>
+            <button className="btn btn-primary">Update</button>
+            <button className="btn btn-secondary">Delete</button>
+          </div>
+        ))}
       </div>
     </div>
   );
